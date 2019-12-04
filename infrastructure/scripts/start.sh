8fb74970f39538e8f8d46e3a49e0209cddc18d80
Max Koval <makov@ciklum.com>
2019-12-04 10:36:04 +0200

    Setup for fpm and nginx

--- infrastructure/scripts/start.sh
+++ infrastructure/scripts/start.sh
@@ -7,3 +7,5 @@ docker-compose -p $NAME_PREFIX  rm -f
 
 docker-compose -p $NAME_PREFIX build --pull
 docker-compose -p $NAME_PREFIX  up -d --force-recreate
+
+docker-compose -p $NAME_PREFIX  exec app-fpm sh -c "cd /var/www; composer service:install"
