71b541a9d3477fbaab4d708c222d595201b8bab3
kovalmax <kovalmax5@gmail.com>
2019-12-25 21:10:37 +0200

    Added node app to the project

---
+++ socket-app/app.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const app = require('express')();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+const port = process.env.SOCKET_PORT;
+
+http.listen(port, () => {
+    console.log('Server listening at port %d', port);
+});
