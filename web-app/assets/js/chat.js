9a86999c75926cf82f0a0a1223adbbf3a1186bce
Max Koval <makov@ciklum.com>
2020-02-12 17:13:52 +0200

    Refactoring for js

--- web-app/assets/js/chat.js
+++ web-app/assets/js/chat.js
@@ -1,114 +1,120 @@
 "use strict";
 (($, io, window) => {
     $(() => {
-        const settings = window.messenger;
         const dom = $(window);
-        const messageInput = $('#messageInput');
-        const messagesContainer = $('#messages-container');
-        const scrollContainer = $('#scroller');
-        messagesContainer.on('scrollToLast', () => scrollToLastMessage(scrollContainer));
-
+        const settings = window.messenger;
         const socket = io.connect(settings.socket.dsn);
-        socket.emit(settings.events.userConnected, {id: settings.user.id, username: settings.user.name});
+        const handler = new ChatHandler(socket, settings, dom);
 
-        const messengerForm = $('#messageForm');
-        if ($('#messages-container > li').length > 7) {
-            scrollToLastMessage(scrollContainer);
-        }
-
-        socket.on(settings.events.userJoined, (data) => console.log('Users stat', data));
-        socket.on(settings.events.message, (message) => appendMessage(messagesContainer, message));
-
-        dom.on('keydown', (event) => {
-            if (event.which === 13 && event.ctrlKey) {
-                sendMessage(messagesContainer, messageInput, settings, socket);
-            }
-        });
-
-        messengerForm.on('submit', (event) => {
-            event.preventDefault();
-            sendMessage(messagesContainer, messageInput, settings, socket);
-        });
+        handler.run();
     });
 
-    /**
-     * @param container Element
-     * @param input Element
-     * @param messenger Object
-     * @param socket SocketIOClient
-     */
-    function sendMessage(container, input, messenger, socket) {
-        let message = input.val();
-        input.val('');
+    function ChatHandler(socket, settings, dom) {
+        const scrollContainer = $('#scroller');
+        const messagesContainer = $('#messageContainer');
+        const messengerForm = $('#messageForm');
+        const messageInput = $('#messageInput');
+        const messageSubmit = $('#messageSubmit');
 
-        message = filterXSS(message).trim();
-        if (message) {
-            message = {username: messenger.user.name, message: message, datetime: new Date()};
-            socket.emit(messenger.events.message, message);
-            appendMessage(container, message);
-            saveMessage(message);
-        }
-    }
+        return {
+            registerEvents() {
+                messagesContainer.on('scrollToLast', () => this.scrollToLastMessage());
 
-    /**
-     * @param container Element
-     * @param message Object
-     */
-    function appendMessage(container, message) {
-        let msg = $('<li>', {class: 'list-group-item message mb-1 rounded-lg border'});
-        msg.append($('<p>', {text: '@'.concat(message.username, ' at ', formatDateTime(message.datetime))}));
-        msg.append($('<p>', {text: message.message}));
+                socket.on(settings.events.userJoined, (data) => console.log('Users stat', data));
 
-        container.append(msg.hide().fadeIn(400));
-        container.trigger('scrollToLast');
-    }
+                socket.on(settings.events.message, (message) => this.appendMessage(message));
 
-    function saveMessage(data) {
-        $.ajax({
-            url: messenger.routes.saveMessage,
-            method: 'POST',
-            dataType: 'json',
-            data: {
-                message: data.message,
-                datetime: data.datetime.toISOString(),
+                dom.on('keydown', (event) => {
+                    if (event.which === 13 && event.ctrlKey) {
+                        this.sendMessage();
+                    }
+                });
+
+                messengerForm.on('submit', (event) => {
+                    event.preventDefault();
+                    this.sendMessage();
+                });
             },
-            success: (res) => {
-                if (parseInt(res.status, 10) > 200) {
-                    console.error('Error saving data with code %s: %s', res.status, res.details);
-                } else {
-                    console.log('Data saved successfully ', res.status);
+            run() {
+                this.registerEvents();
+                this.emitEvent(settings.events.userConnected, {id: settings.user.id, username: settings.user.name});
+                if (messagesContainer.find('li').length > 7) {
+                    this.scrollToLastMessage();
                 }
             },
-            error: (err) => {
-                console.error('Server error. ', err);
-            }
-        });
-    }
+            sendMessage() {
+                let message = messageInput.val();
+                messageInput.val('');
 
-    function scrollToLastMessage(element) {
-        element[0].scrollTop = element[0].scrollHeight;
-    }
+                message = filterXSS(message).trim();
+                if (message) {
+                    message = {username: messenger.user.name, message: message, datetime: new Date()};
+                    this.emitEvent(messenger.events.message, message);
+                    this.appendMessage(message);
+                    this.saveMessage(message);
+                }
+            },
+            appendMessage(message) {
+                let msg = $('<li>', {class: 'list-group-item message mb-1 rounded-lg border'});
+                msg.append($('<p>', {text: '@'.concat(message.username, ' at ', this.formatDateTime(message.datetime))}));
+                msg.append($('<p>', {text: message.message}));
 
-    function formatDateTime(datetime) {
-        if (!(datetime instanceof Date)) {
-            datetime = new Date(datetime);
-        }
-        let year = datetime.getFullYear();
-        let month = addLeadingZeroes(datetime.getMonth() + 1);
-        let day = addLeadingZeroes(datetime.getDate());
-        let hour = addLeadingZeroes(datetime.getHours());
-        let minute = addLeadingZeroes(datetime.getMinutes());
-        let sec = addLeadingZeroes(datetime.getSeconds());
+                messagesContainer.append(msg.hide().fadeIn(400));
+                messagesContainer.trigger('scrollToLast');
+            },
+            saveMessage(data) {
+                $.ajax({
+                    url: settings.routes.saveMessage,
+                    method: 'POST',
+                    dataType: 'json',
+                    beforeSend: () => {
+                        messageSubmit.html('<span class="spinner-grow spinner-grow" role="status" aria-hidden="true" />Loading...');
+                        messageSubmit.attr('disabled', 'disabled');
+                    },
+                    complete: () => {
+                        messageSubmit.html('Send');
+                        messageSubmit.removeAttr('disabled');
+                    },
+                    data: {message: data.message, datetime: data.datetime.toISOString()},
+                    success: (res) => {
+                        if (parseInt(res.status, 10) > 200) {
+                            console.error('Error saving data with code %s: %s', res.status, res.details);
+                        } else {
+                            console.log('Data saved successfully ', res.status);
+                        }
+                    },
+                    error: (err) => {
+                        console.error('Server error. ', err);
+                    }
+                });
+            },
+            emitEvent(eventName, event) {
+                socket.emit(eventName, event);
+            },
+            scrollToLastMessage() {
+                scrollContainer[0].scrollTop = scrollContainer[0].scrollHeight;
+            },
+            formatDateTime(datetime) {
+                if (!(datetime instanceof Date)) {
+                    datetime = new Date(datetime);
+                }
+                let year = datetime.getFullYear();
+                let month = this.addLeadingZeroes(datetime.getMonth() + 1);
+                let day = this.addLeadingZeroes(datetime.getDate());
+                let hour = this.addLeadingZeroes(datetime.getHours());
+                let minute = this.addLeadingZeroes(datetime.getMinutes());
+                let sec = this.addLeadingZeroes(datetime.getSeconds());
 
-        return `${day}/${month}/${year} ${hour}:${minute}:${sec}`;
-    }
+                return `${day}/${month}/${year} ${hour}:${minute}:${sec}`;
+            },
+            addLeadingZeroes(number) {
+                if (number <= 9) {
+                    number = '0' + number;
+                }
 
-    function addLeadingZeroes(number) {
-        if (number <= 9) {
-            number = '0' + number;
-        }
-
-        return number;
+                return number;
+            },
+        };
     }
 })(window.jQuery, window.io, window);
 
